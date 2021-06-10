fbb3b76b91bb04267f1d054de4005c4a226019fb
Hyaguinhogp <hyago.v.a.m@gmail.com>
2021-06-10 07:21:48 -0400

    User CRUD

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
@@ -0,0 +1,73 @@
+package com.devsuperior.dscatalog.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll(){
+		List<UserDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/paged")
+	public ResponseEntity<Page<UserDTO>> findAllPaged(Pageable pageable) {
+		Page<UserDTO> page = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(page);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable long id) {
+		UserDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> insert(@RequestBody UserDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> update(@PathVariable long id, @RequestBody UserDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> delete(@PathVariable long id) {
+		try {
+			service.delete(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.noContent().build();
+	}
+} 
---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/UserService.java
@@ -0,0 +1,89 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.dto.RoleDTO;
+import com.devsuperior.dscatalog.dto.UserDTO;
+import com.devsuperior.dscatalog.entities.User;
+import com.devsuperior.dscatalog.repositories.RoleRepository;
+import com.devsuperior.dscatalog.repositories.UserRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourcesNotFoundException;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@Transactional(readOnly = true)
+	public List<UserDTO> findAll() {
+		List<UserDTO> list = repository.findAll().stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return list;
+	}
+	
+	@Transactional(readOnly = true)
+	public Page<UserDTO> findAllPaged(Pageable pageable){
+		Page<User> page = repository.findAll(pageable);
+		return page.map(x -> new UserDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public UserDTO findById(long id) {
+		Optional<User> obj = repository.findById(id);
+		User user = obj.orElseThrow(() -> new ResourcesNotFoundException("Entity not found"));
+		return new UserDTO(user);
+	}
+	
+	@Transactional
+	public UserDTO insert(UserDTO dto) {
+		User user = new User();
+		dtoToUser(dto, user);
+		user.setPassword(passwordEncoder.encode(dto.getPassword()));
+		repository.save(user);
+		return new UserDTO(user);
+		
+	}
+
+	@Transactional
+	public UserDTO update(long id, UserDTO dto) {
+		User user = new User();
+		dtoToUser(dto, user);
+		user.setId(id);
+		repository.save(user);
+		return new UserDTO(user);
+	}
+	
+	@Transactional
+	public void delete(long id) {
+		repository.deleteById(id);
+
+	}
+	
+	@Transactional
+	
+	private void dtoToUser(UserDTO dto, User user) {
+		user.setFirstName(dto.getFirstName());
+		user.setLastName(dto.getLastName());
+		user.setEmail(dto.getEmail());
+		
+		user.getRoles().clear();
+		for(RoleDTO entity : dto.getRoles()) {
+			user.getRoles().add(roleRepository.getOne(entity.getId()));
+		}
+	}
+}
